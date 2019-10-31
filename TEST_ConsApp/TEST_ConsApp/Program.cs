03114e5600ae03dd1d9c579612a15e436ad3bdbc
Lyralei16 <44676382+Lyralei16@users.noreply.github.com>
2019-10-31 18:03:30 +0100

    added files
    Just adding local files to work with

---
+++ TEST_ConsApp/TEST_ConsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TEST_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<String> Customers = new List<string>();
+
+            //Customers.Add("Ching");
+            //Customers.Add("Chang");
+            //Customers.Add("Chong");
+
+            //Console.WriteLine(Customers.Count);
+
+            //foreach (var i in Customers)
+            //{
+            //    Console.WriteLine(i);
+         
+            //}
+
+
+        }
+    }
+}
