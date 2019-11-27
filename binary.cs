ca5482d81e90393cc6b36f8235662ee57d60e25a
Lyralei16 <44676382+Lyralei16@users.noreply.github.com>
2019-11-27 02:10:17 +0100

    Create binary.cs

---
+++ binary.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] binary = new int[]{};
+
+        if (n > 0){
+            Console.WriteLine(binaryCount(n));
+            
+        }
+
+    }
+    static int binaryCount(int n){
+        int part = n % 2;
+            
+        Console.WriteLine(n);
+        return part;
+    }
+}
