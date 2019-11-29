9877fcbda4bf0d8784ed60bbbebad5d811c364fb
Lyralei16 <44676382+Lyralei16@users.noreply.github.com>
2019-11-29 17:29:22 +0100

    binary comp

--- binary.cs
+++ binary.cs
@@ -18,18 +18,34 @@ class Solution {
 
     static void Main(string[] args) {
         int n = Convert.ToInt32(Console.ReadLine());
-        int[] binary = new int[]{};
+        int[] binary = new int[10];
+        int i;
 
-        if (n > 0){
-            Console.WriteLine(binaryCount(n));
+         for(i=0; n>0; i++)      
+        {      
+         binary[i]=n%2;      
+         n= n/2;    
+        }  
+
+       Console.Write("Binary of the given number= ");      
+       for(i=i-1 ;i>=0 ;i--)      
+       {      
+        Console.Write(binary[i]);      
+       }                 
+
+     /*    if (n != 0){
+            int part = n % 2;
+            //Console.WriteLine(binaryCount(n));
+            binary = + part;
+            Console.WriteLine(binary);
             
-        }
+        } */
 
     }
-    static int binaryCount(int n){
-        int part = n % 2;
+    /* static int binaryCount(int n){
+        
             
         Console.WriteLine(n);
         return part;
-    }
+    } */
 }
