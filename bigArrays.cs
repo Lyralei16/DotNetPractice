be71e83b5e0f7b5527dbd6ff3a0ab041ad463f4f
Lyralei16 <44676382+Lyralei16@users.noreply.github.com>
2019-11-30 20:37:45 +0100

    kk

--- bigArrays.cs
+++ bigArrays.cs
@@ -23,28 +23,29 @@ class Solution {
 
         for (int i = 0; i < 6; i++) {
             arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
-           
-           
+                 
         }
+        arrHG(arr);
 
     }
-     static int[,] arrHG(int[][] arr){
+     static int[][] arrHG(int[][] arr){
 
-        int[,] hourGlass = new int[3, 3];
+        int[][] hourGlass = new int[3][];
 
         for (int i = 0; i < 3; i++){
 
             for (int j = 0; j <3; j++){
                 if( i == 0 && j == 1 || i == 2 && j == 1){
-                    hourGlass[i][j] = " ";
+                    hourGlass[i][j] = 0;
                 } else
                 {
                     hourGlass[i][j] = arr[i][j];
                 }
-               
-            }
+               Console.WriteLine(hourGlass[i][j]);
+            } 
         }
 
         return hourGlass;
     }
+    
 }
